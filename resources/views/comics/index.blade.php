7413fd58594caf3d133040fff9db36878e403090
PinoGabriel <pinogabriel01@gmail.com>
2024-01-26 16:24:26 +0100

    feat: added method delate

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -8,9 +8,14 @@
                 <div class="myCard">
                     <img class="myImgThumb" src={{ $item->thumb }} alt="">
                     <p class="text-white pt-2">{{ strtoupper($item->title) }}</p>
-                    <div class="d-flex">
-                        <a class="btn btn-success" href="{{ route('comics.show', $item->id) }}">Details</a>
-                        <a class="btn btn-primary mx-2" href="{{ route('comics.edit', $item->id) }}">Edit</a>
+                    <div class="d-flex flex-wrap">
+                        <a class="btn btn-success mb-2" href="{{ route('comics.show', $item->id) }}">Details</a>
+                        <a class="btn btn-primary mx-2 mb-2" href="{{ route('comics.edit', $item->id) }}">Edit</a>
+                        <form action="{{ route('comics.destroy', $item->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" value="delete" class="btn btn-danger d-inline-block">
+                        </form>
                     </div>
                 </div>
             @endforeach
